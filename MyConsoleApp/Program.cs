874109f6bf5dd219cbfc342c006894708df0e106
abemore11 <abr.moreno@outlook.com>
2026-04-30 20:31:18 -0600

    1st assignment

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+
+// 1. Define "Now" and "Christmas"
+DateTime today = DateTime.Today;
+DateTime christmas = new DateTime(today.Year, 12, 25);
+
+// Uses string interpolation
+Console.WriteLine($"The current time is {today}");
+
+// 2. Subtract the dates to get a TimeSpan
+TimeSpan difference = christmas - today;
+
+// 3. Output the result using string interpolation
+Console.WriteLine($"There are {difference.Days} days until the next Christmas!");
